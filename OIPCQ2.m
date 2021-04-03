b9b5114c78fd0ce4a3564d2d5f6372dd7b92da72
haammim <59698610+haammim@users.noreply.github.com>
2021-04-04 03:27:57 +0430

    Update OIPCQ2.m

--- OIPCQ2.m
+++ OIPCQ2.m
@@ -1,30 +1,32 @@
 data22=load('Dream3_10.txt');
 data=data22';
-%%data=load('AML.txt');
-n_gene=size(data,1);
+
+% data=gene_expression_log;
+% TrueNet=golden_network_matrix_diag;
+
 TrueNet=load('TrueNetwork3_10.txt');
 TrueNetUper=triu(TrueNet,1);
-UndirectCompareFinal=["theta0", 'theta1', 'psi1', 'theta2', 'psi2', 'TP', 'FP', 'TN', 'FN', 'PPV', 'TPR', 'ACC', 'F', 'TNR', 'FNR', 'FPR', 'NPV', 'FDR', 'MCC'];
-theta0=0.1;
-theta1=0.01;
-theta2=0.01;
-psi1=70;
-psi2=70;
 
+UndirectCompareFinal=["theta0", 'theta', 'psi', 'TP', 'FP', 'TN', 'FN', 'PPV', 'TPR', 'ACC', 'F', 'TNR', 'FNR', 'FPR', 'NPV', 'FDR', 'MCC'];
+
+theta0=0.1;
+theta=0.01;
+psi=70;
+
+n_gene=size(data,1);
 
 G=ones(n_gene,n_gene);
 G=tril(G,-1)';
+
 Gval={};
+
 nn=size(G,1);
+
 UndirectCompare=[];
 
 
-
 for orderr=1:3
-    
-    order=orderr-1;
-    
-    if order==0
+    if orderr==1
         for i=1:nn-1
             for j=i+1:nn
                 Gval{1,orderr}{i,j}=cmi(data(i,:),data(j,:));
@@ -32,95 +34,120 @@ for orderr=1:3
         end
         for i=1:nn-1
             for j=i+1:nn
-                if Gval{1,orderr}{i,j}<theta0
+                if Gval{1,1}{i,j}<theta0
                     G(i,j)=0;
                 end
             end
         end
         
-    elseif order==1
+    elseif orderr==2
         for i=1:nn-1
             for j=i+1:nn
                 if G(i,j)==1
-                cmivec=[];
-                for k=1:nn
-                    if k~=i && k~=j
-                        if G(i,k)==1 || G(k,i)==1
-                            if G(j,k)==1 || G(k,j)==1
-                                cmiv=MI2(data(i,:), data(j,:), data(k,:));
-                                cmivec=[cmivec, cmiv];
+                    for k=1:nn
+                        if k~=i && k~=j
+                            if G(i,k)==1 || G(k,i)==1
+                                if G(j,k)==1 || G(k,j)==1
+                                    Gval{1,orderr}{i,j}{1,k}=MI2(data(i,:), data(j,:), data(k,:));
+                                end
                             end
                         end
                     end
                 end
-                Gval{1,orderr}{i,j}=cmivec;
-                end
             end
         end
+        prc=[];
         for i=1:nn-1
             for j=i+1:nn
+                Vecqnt=[];
                 if G(i,j)==1
-                    qnt1=prctile(Gval{1,orderr}{i,j},psi1);
-                    if qnt1<theta1
-                        G(i,j)=0;
-                    end
-                end
-            end
-        end
-        
-    elseif order==2
-        for i=1:nn-1
-            for j=i+1:nn
-                if G(i,j)==1
-                cmivec=[];
-                for k=1:nn-1
-                    if k~=i && k~=j
-                    for l=k+1:nn
-                        if l~=i && l~=j
-                        if G(i,l)==1 || G(j,l)==1 || G(l,i)==1 || G(l,j)==1
-                            if G(i,k)==1 || G(j,k)==1 || G(k,i)==1 || G(k,j)==1
-                                cmiv=MI2(data(i,:), data(j,:), data([k,l],:));
-                                cmivec=[cmivec, cmiv];
+                    for k=1:nn
+                        if k~=i && k~=j
+                            if G(i,k)==1 || G(k,i)==1
+                                if G(j,k)==1 || G(k,j)==1
+                                    Vecqnt=[Vecqnt, Gval{1,2}{i,j}{1,k}];
+                                end
                             end
                         end
-                        end
-                    end
                     end
                 end
-                Gval{1,orderr}{i,j}=cmivec;
-                end
+                prc(i,j)=prctile(Vecqnt, psi);
             end
         end
         for i=1:nn-1
             for j=i+1:nn
-                if G(i,j)==1
-                    qnt2=prctile(Gval{1,orderr}{i,j},psi2);
-                    if qnt2<theta2
-                        G(i,j)=0;
-                    end
+                if prc(i,j)<theta
+                    G(i,j)=0;
                 end
             end
         end
         
+    else
+        for i=1:nn-1
+            for j=i+1:nn
+                if G(i,j)==1
+                    for k=1:nn-1
+                        if k~=i && k~=j
+                            for l=k+1:nn
+                                if l~=i && l~=j
+                                    if G(i,k)==1 || G(k,i)==1 || G(j,k)==1 || G(k,j)==1
+                                        if G(i,l)==1 || G(l,i)==1 || G(j,l)==1 || G(l,j)==1
+                                            Gval{1,orderr}{i,j}{1,k}{1,l}=MI2(data(i,:), data(j,:), data([k,l],:));
+                                        end
+                                    end
+                                end
+                            end
+                        end
+                    end
+                end
+            end
+        end
+        prc=[];
+        for i=1:nn-1
+            for j=i+1:nn
+                Vecqnt=[];
+                if G(i,j)==1
+                    for k=1:nn
+                        for l=k+1:nn
+                            if k~=i && k~=j && l~=i && l~=j
+                                if G(i,k)==1 || G(k,i)==1 || G(j,k)==1 || G(k,j)==1
+                                    if G(i,l)==1 || G(l,i)==1 || G(j,l)==1 || G(l,j)==1
+                                        Vecqnt=[Vecqnt, Gval{1,3}{i,j}{1,k}{1,l}];
+                                    end
+                                end
+                            end
+                        end
+                    end
+                end
+                prc(i,j)=prctile(Vecqnt, psi);
+            end
+        end
+        for i=1:nn-1
+            for j=i+1:nn
+                if prc(i,j)<theta
+                    G(i,j)=0;
+                end
+            end
+        end
     end
     
 end
-
 
 % *************************************************************************
 % This section should be use for AML dataset
 % *************************************************************************
 
-% for i=1:size(Target_Genes,1)-1
-%     for j=i+1:size(Target_Genes,1)
-%         G(Target_Genes(i,1),Target_Genes(j,1))=0;
-%         G(Target_Genes(j,1),Target_Genes(i,1))=0;
+%     for i=1:size(targets,1)-1
+%         for j=i+1:size(targets,1)
+%             G(targets(i,1),targets(j,1))=0;
+%             G(targets(j,1),targets(i,1))=0;
+%         end
 %     end
-% end
 
 % *************************************************************************
 % END
 % *************************************************************************
 
 UndirectCompare=Compare(TrueNetUper,G,nn);
-UndirectCompare=[theta0, theta1, psi1, theta2, psi2, UndirectCompare];
+UndirectCompare=[theta0, theta, psi, UndirectCompare];
+UndirectCompareFinal=[UndirectCompareFinal; UndirectCompare];
